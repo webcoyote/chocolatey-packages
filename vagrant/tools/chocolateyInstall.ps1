baed963be40d528ea520965ea4d7125c1ed34623
Patrick Wyatt <pat@codeofhonor.com>
2013-04-24 11:41:11 -0700

    Updated vagrant to 1.2.2

--- vagrant/tools/chocolateyInstall.ps1
+++ vagrant/tools/chocolateyInstall.ps1
@@ -1,29 +1,4 @@
-try {
-  # Vagrant 1.1.5
-  $url = 'http://files.vagrantup.com/packages/64e360814c3ad960d810456add977fd4c7d47ce6/Vagrant.msi'
-  $packageName = 'vagrant'
-  $installerType = 'msi'
-  $url64 = $url
-  $silentArgs = '/quiet'
-  $validExitCodes = @(0)
-
-  # Configure install location
-  if ($env:chocolatey_bin_root -ne $null) {
-    # Set installation directory to user-configured bin directory
-    $installDir = join-path $env:systemdrive $env:chocolatey_bin_root
-  }
-  else {
-    # Set installation directory to C:\Bin
-    $installDir = join-path $env:systemdrive "\bin"
-  }
-  $env:chocolateyInstallArguments = " INSTALLDIR=`"" + $installDir + "`" "
-
-  # Run MSI installer
-  Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
-
-  Write-ChocolateySuccess '__NAME__'
-}
-catch {
-  Write-ChocolateyFailure '__NAME__' $($_.Exception.Message)
-  throw 
-}
+$url = 'http://files.vagrantup.com/packages/7e400d00a3c5a0fdf2809c8b5001a035415a607b/Vagrant_1.2.2.msi'
+$url64 = $url
+$validExitCodes = @(0)
+Install-ChocolateyPackage "vagrant" "msi" "/quiet" "$url" "$url64"  -validExitCodes $validExitCodes
