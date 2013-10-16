79d1c0c1fde65db7a31a0a0dc38026d44bb06f3f
Rogerio Prado de Jesus <rogeriopradoj@gmail.com>
2013-10-16 00:48:00 -0300

    Update Vagrant download url to 1.3.5 - #13
    Signed-off-by: Rogerio Prado de Jesus <rogeriopradoj@gmail.com>

--- vagrant/tools/chocolateyInstall.ps1
+++ vagrant/tools/chocolateyInstall.ps1
@@ -1,4 +1,4 @@
-$url = 'http://files.vagrantup.com/packages/0ac2a87388419b989c3c0d0318cc97df3b0ed27d/Vagrant_1.3.4.msi'
+$url = 'http://files.vagrantup.com/packages/a40522f5fabccb9ddabad03d836e120ff5d14093/Vagrant_1.3.5.msi'
 $url64 = $url
 $validExitCodes = @(0)
 Install-ChocolateyPackage "vagrant" "msi" "/quiet" "$url" "$url64"  -validExitCodes $validExitCodes
