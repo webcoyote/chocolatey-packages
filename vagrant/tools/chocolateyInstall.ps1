1f95b8fa606486549755268e674bd701638469db
Patrick Wyatt <pat@codeofhonor.com>
2013-12-09 14:08:28 -0800

    Update to vagrant 1.4.0

--- vagrant/tools/chocolateyInstall.ps1
+++ vagrant/tools/chocolateyInstall.ps1
@@ -1,4 +1,4 @@
-$url = 'http://files.vagrantup.com/packages/a40522f5fabccb9ddabad03d836e120ff5d14093/Vagrant_1.3.5.msi'
+$url = 'https://dl.bintray.com/mitchellh/vagrant/Vagrant_1.4.0.msi'
 $url64 = $url
 $validExitCodes = @(0)
 Install-ChocolateyPackage "vagrant" "msi" "/quiet" "$url" "$url64"  -validExitCodes $validExitCodes
