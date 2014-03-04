d7213268a0e2648df8ae5267e37768c026173b1b
webcoyote <pat@codeofhonor.com>
2014-03-03 22:22:32 -0800

    Update golang to 1.2.1

--- golang/tools/chocolateyInstall.ps1
+++ golang/tools/chocolateyInstall.ps1
@@ -1,31 +1,13 @@
-# Go [64 bit] [windows] [version 1.2]
-$url = 'https://go.googlecode.com/files/go1.2.windows-amd64.zip'
+$packageName = 'golang'
+$url = 'https://go.googlecode.com/files/go1.2.1.windows-amd64.msi'
+$url64 = $url
+$validExitCodes = @(0)
 
-# Configure install location
-$dst = join-path $env:systemdrive $env:chocolatey_bin_root
-
-# Install it
-Install-ChocolateyZipPackage 'golang' $url $dst
-
-# AppendPath ";a;b;;c;" ";d;"    => a;b;c;d
-function AppendPath ([String]$path, [String]$dir) {
-  $result = $path.split(';') + $dir.split(';') |
-      where { $_ -ne '' } |
-      select -uniq
-  [String]::join(';', $result)
+try {
+  Install-ChocolateyPackage "$packageName" "msi" "/quiet" "$url" "$url64" -validExitCodes $validExitCodes
+  Write-ChocolateySuccess "$packageName"
 }
-
-# Add to the global environment; $target => { 'Machine', User' }
-function AddGlobalPath ([String]$dir, [String]$target) {
-  $path = [Environment]::GetEnvironmentVariable('Path', $target)
-  $path = AppendPath $path $dir
-  [Environment]::SetEnvironmentVariable('Path', $path, $target)
+catch {
+  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
+  throw
 }
-
-# Configure go environment variables
-$go_root_dir = join-path $dst "go"
-[Environment]::SetEnvironmentVariable('GOROOT', $go_root_dir, "User")
-
-# Add go bin directory to the path
-$go_bin_dir  = join-path $go_root_dir "bin"
-AddGlobalPath $go_bin_dir "User"
