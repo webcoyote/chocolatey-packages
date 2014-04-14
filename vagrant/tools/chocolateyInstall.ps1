bb70e8521eb98dea1f92549afff15c29d6cc64aa
Pablo Crivella <pablocrivella@gmail.com>
2014-04-14 09:18:02 -0400

    update to vagrant 1.5.2

--- vagrant/tools/chocolateyInstall.ps1
+++ vagrant/tools/chocolateyInstall.ps1
@@ -1,4 +1,4 @@
-$url = 'https://dl.bintray.com/mitchellh/vagrant/vagrant_1.5.1.msi'
+$url = 'https://dl.bintray.com/mitchellh/vagrant/vagrant_1.5.2.msi'
 $url64 = $url
 $validExitCodes = @(0)
 Install-ChocolateyPackage "vagrant" "msi" "/quiet" "$url" "$url64"  -validExitCodes $validExitCodes
