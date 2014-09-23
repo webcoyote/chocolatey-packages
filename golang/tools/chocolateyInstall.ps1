0e1a0b2ff9415543f910df6a3faaaf30729fd9be
Frédéric Boyer <fboyer@users.noreply.github.com>
2014-09-23 18:00:11 -0400

    Fix architecture and bump golang version to 1.3.1
    Added the golang 386 architecture install path
    Updated the golang download links to point to the golang version 1.3.1 installers

--- golang/tools/chocolateyInstall.ps1
+++ golang/tools/chocolateyInstall.ps1
@@ -1,10 +1,11 @@
 $packageName = 'golang'
-$url = 'https://storage.googleapis.com/golang/go1.2.2.windows-amd64.msi'
-$url64 = $url
+$installerType = 'msi'
+$url = 'https://storage.googleapis.com/golang/go1.3.1.windows-386.msi'
+$url64 = 'https://storage.googleapis.com/golang/go1.3.1.windows-amd64.msi'
 $validExitCodes = @(0)
 
 try {
-  Install-ChocolateyPackage "$packageName" "msi" "/quiet" "$url" "$url64" -validExitCodes $validExitCodes
+  Install-ChocolateyPackage "$packageName" "$installerType" "/quiet" "$url" "$url64" -validExitCodes $validExitCodes
   Write-ChocolateySuccess "$packageName"
 }
 catch {
