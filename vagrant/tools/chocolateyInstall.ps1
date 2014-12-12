bf2aae62009d3189ae7f7421c20b8845d6cb1484
Patrick Wyatt <pat@codeofhonor.com>
2014-12-11 16:53:41 -0800

    vagrant 1.7.0

--- vagrant/tools/chocolateyInstall.ps1
+++ vagrant/tools/chocolateyInstall.ps1
@@ -1,4 +1,4 @@
-$url = 'https://dl.bintray.com/mitchellh/vagrant/vagrant_1.6.5.msi'
+$url = 'https://dl.bintray.com/mitchellh/vagrant/vagrant_1.7.0.msi'
 $url64 = $url
 $validExitCodes = @(0)
 Install-ChocolateyPackage "vagrant" "msi" "/quiet" "$url" "$url64"  -validExitCodes $validExitCodes
